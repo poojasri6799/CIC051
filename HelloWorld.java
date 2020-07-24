3f86b2cc920d64c8a3bf32a82162e3181faa40db
poojasri6799 <68681037+poojasri6799@users.noreply.github.com>
2020-07-24 13:14:38 +0530

    Update HelloWorld.java
    update Helloworld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,7 @@
 public class HelloWorld{
 public static void main(String args[]){
-while(true){
-System.out.println("Hello fuhgufhb ofjgif World");
-  
-try{
-Thread.sleep(60000);
-}catch(Exception ex){
-System.out.println(ex);
-}
-}
+System.out.println("Hello World");
+System.out.println("Hello, This change done by some other developer");
+
 }
 }
