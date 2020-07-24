3284bfe83e253bcebb8bd0e25268f2cb16bfd832
poojasri6799 <68681037+poojasri6799@users.noreply.github.com>
2020-07-24 12:48:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
 public class HelloWorld{
 public static void main(String args[]){
 while(true){
-System.out.println("Hello World");
+System.out.println("Hello fuhgufhb ofjgif World");
+  
 try{
 Thread.sleep(60000);
 }catch(Exception ex){
