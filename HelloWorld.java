c582a3c01645c6aa632a15d9efd18d437c0adae9
poojasri <poojasrijampani@gmail.com>
2020-07-27 12:36:35 +0530

    gitignore demo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld
 {
 	public static void main(String args[]){
-	System.out.println("this is my master branch");
+	System.out.println("this is my master application");
 	System.out.println("this is my new feature implementation");
 	System.out.println("this is my another feature developed in feature branch");
 
